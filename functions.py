0edaf8c26225b448c01b07bd2e9d09a969f56a6d
Kash <amahlw@gmail.com>
2020-09-06 23:54:55 -0400

    Initial commit

---
+++ functions.py
@@ -0,0 +1,38 @@
+
+#q1
+
+def area(width, height):
+    result = width * height
+    return result
+
+
+#q2
+result = area(5, 6)
+
+#print(result)
+
+
+#q3
+result = area(10,5)
+result = area(5,5)
+
+#q4
+def subtract(num1,num2):
+     result = num1 - num2
+     return result
+
+#q5
+result = subtract(25,5)
+print(result)
+result = subtract(100,52)
+print(result)
+
+#q7
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+results = add(100,150)
+print(results)
+result = add(500,150)
+print(result)
